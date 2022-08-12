77c27aa237c842d61773cf0711ab124e6c95046b
srinivasg439 <srinivasg439@gmail.com>
2022-08-12 11:44:46 +0530

    Project Completed

--- setup.py
+++ setup.py
@@ -8,21 +8,30 @@ AUTHOR="Srinivas"
 DESCRIPTION="This is the First Machine Learing Project"
 REQUIREMENT_FILE_NAME="requirements.txt"
 
-def get_requirements_list()->List[str]:
+HYPHEN_E_DOT = "-e ."
+
+
+def get_requirements_list() -> List[str]:
     """
-    This function is going to return list of requirement
+    Description: This function is going to return list of requirement
     mention in requirements.txt file
-
-    return This function is goint to return a list which will contain name of libraries mentioned in requirements.txt file
-
+    return This function is going to return a list which contain name
+    of libraries mentioned in requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        requirement_file.readlines()
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
+
+
+
 setup(
-    name=PROJECT_NAME,
-    version=VERSION,
-    author=AUTHOR,
-    description=DESCRIPTION,
-    packages=find_packages(),
-    install_requires=get_requirements_list()
-    )
+name=PROJECT_NAME,
+version=VERSION,
+author=AUTHOR,
+description=DESCRIPTION,
+packages=find_packages(), 
+install_requires=get_requirements_list()
+)
